cbddef4696e7bb45ce41a276c2187ef370ab465a
DevMalwar <150520059+DevMalwar@users.noreply.github.com>
2024-03-23 17:17:59 +0600

    Add files via upload

---
+++ PocemonApiOtbor/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "./featuers/postsSlice";
+import './App.css'
+
+const App = () => {
+    const pokemon = useSelector((state) => state.postsSlice.pokemon)
+    const dispatch = useDispatch()
+    console.log(pokemon)
+    useEffect(() => {
+        dispatch(fetchData())
+    }, []);
+    return (
+        <div className='main'>
+            <h1 className='title'>Pocemon Geeks</h1>
+            {
+                pokemon.map((item) => (
+                    <div key={item.id} className='card' >
+                        <img width='130px' src={item.sprites.other.dream_world.front_default} alt=""/>
+                        <h2>{item.name}</h2>
+                    </div>
+                ))
+            }
+        </div>
+    );
+};
+
+export default App;
